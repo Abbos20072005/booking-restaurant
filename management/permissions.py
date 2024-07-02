ed212dedbca69ddb8018e3f4e5ea315d392b50b7
Farruh64 <rixsiboyevfaruuh@gmail.com>
2024-07-02 18:59:23 +0500

    some changes

--- management/permissions.py
+++ management/permissions.py
@@ -1,27 +1,25 @@
 from rest_framework import permissions
-from .models import Manager
+from .models import Manager, Administrator
 
 
 class IsManager(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.status == 3 and Manager.objects.filter(restaurant_id=obj.id).exists():
-            return True
-        return False
 
-    def has_permission(self, request, view):
-        if request.user.status == 3:
-            return True
-        return False
+
+    def has_object_permission(self, request, view, obj):
+
+        return request.user.status == 3 and Manager.objects.filter(user_id=request.user.id,
+                                                                   restaurant_id=obj.id).exists()
 
 
 class IsManagerOrAdministrator(permissions.BasePermission):
+
+
     def has_object_permission(self, request, view, obj):
-        if request.user.status == 4:
-            return True
-        return False
 
-    def has_permission(self, request, view):
+        if request.user.status == 3:
+            return Manager.objects.filter(user_id=request.user.id, restaurant_id=obj.id).exists()
 
         if request.user.status == 4:
-            return True
+            return Administrator.objects.filter(user_id=request.user.id, manager__restaurant_id=obj.id).exists()
+
         return False
