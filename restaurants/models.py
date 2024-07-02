2e63014c2035b69ca8a7b9a73f402411fd9a7dee
abbos <hikmatullaevabbos24@gmail.com>
2024-07-02 10:23:45 +0500

    mixins file deleted, images fields changed in models file

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,6 @@ from authentication.models import User
 from .utils import name_validator, validate_uzb_number
 
 
-
 class RestaurantCategory(models.Model):
     name = models.CharField(max_length=100,
                             validators=[name_validator], unique=True)  # pan-asian, europe, usa, arabic, turkish, family
@@ -14,15 +13,21 @@ class RestaurantCategory(models.Model):
         return self.name
 
 
+class RestaurantImages(models.Model):
+    images = models.ImageField(upload_to='images/restaurant')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.images
+
+
 class Restaurant(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    picture = models.ImageField(upload_to='images/restaurants/',
-                                default='images/restaurants/default_restaurant.jpg')  # default= need to add
     description = models.TextField(blank=True, null=True)
     service_fee = models.FloatField(default=0.0)
     balance = models.FloatField(default=0.0)
-
 
     booking_count_total = models.IntegerField(default=0)
     booking_count_day_by_day = models.IntegerField(default=0)
@@ -30,9 +35,12 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=100, blank=True, null=True)
     phone = models.CharField(max_length=13, validators=[validate_uzb_number])
     email = models.CharField(max_length=100, blank=True, null=True)
-    # location = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=2000, blank=True, null=True)
+    longitude = models.CharField(max_length=2000, blank=True, null=True)
 
     category = models.ForeignKey(RestaurantCategory, on_delete=models.CASCADE)
+    picture = models.ForeignKey(RestaurantImages, on_delete=models.CASCADE,
+                                default='images/restaurant/default_restaurant.jpg')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -51,15 +59,23 @@ class RoomType(models.Model):
         return self.name
 
 
+class RoomImages(models.Model):
+    images = models.ImageField(upload_to='images/room')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.images
+
+
 class RestaurantRoom(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, validators=[name_validator], unique=True)
     description = models.TextField(blank=True, null=True)
-    pictures = models.ImageField(upload_to='images/restaurants/room_images/',
-                                 default='images/restaurants/room_images/default_room.jpg')  # default= need to add
+    pictures = models.ForeignKey(RoomImages, on_delete=models.CASCADE, default='images/room/default_room.jpg')
 
     people_number = models.IntegerField(default=0)
-    room_type = models.ManyToManyField(RoomType)
+    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -84,8 +100,8 @@ class RestaurantMenu(models.Model):
 
     name = models.CharField(max_length=255, validators=[name_validator], unique=True)
     description = models.TextField(blank=True, null=True)
-    pictures = models.ImageField(upload_to='images/restaurants/menu_images/',
-                                 default='images/restaurants/menu_images/default_menu.jpg', blank=True)
+    pictures = models.ImageField(upload_to='images/menu',
+                                 default='images/menu/default_menu.jpg')
     ingredients = models.TextField(max_length=2000, blank=True, null=True)
     price = models.FloatField(default=0.0)
 
